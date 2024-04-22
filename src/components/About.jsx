3366bc66548861099c12ea76168effafe9473384
avocardiojam <katerina_sret@icloud.com>
2024-04-22 21:43:52 +0300

    add About

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import styles from '../styles/About.module.css'
+
+const About = () => {
+    return (
+        <section className={styles.about}>
+            <div className={styles.container}>
+                <p className={styles.title}>/ ABOUT</p>
+                <p className={styles.description}>KRISTINA IS A MEMBER OF THE CHARTERED INSTITUTE OF ARBITRATORS (CIARB) AND HAS BEEN RECOGNISED IN INTERNATIONAL ARBITRATION BY LEADING DIRECTORIES INCLUDING, CHAMBERS EUROPE, CHAMBERS GLOBAL.</p>
+            </div>
+        </section>
+    );
+};
+export default About ;
