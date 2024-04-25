2703a6e03be4bfe8e9d3fc059707db7ee9dfa8b1
avocardiojam <katerina_sret@icloud.com>
2024-04-25 22:32:50 +0300

    add some styles

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -5,14 +5,28 @@ const Head = () => {
     return (
         <section className={styles.head}>
             <div className={styles.container}>
-                <div className={styles.menu}>
-                    <ul className={styles.menuList}>
-                        {MenuData.map((el) => (
-                            <li className={styles.menuItem}>
-                                <a className={styles.menuLink} href={el.href}>{el.title}</a>
-                            </li>
-                        ))}
-                    </ul>
+                <div className={styles.items}>
+                    <div className={styles.first}>
+                        <img src="./image/first.svg" alt="" />
+                        <p>2024</p>
+                    </div>
+                    <div className={styles.main}>
+                        <div className={styles.menu}>
+                            <ul className={styles.menuList}>
+                                {MenuData.map((el) => (
+                                    <li className={styles.menuItem}>
+                                        <a className={styles.menuLink} href={el.href}>{el.title}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className={styles.title}>
+                            <p>KRISTINA SRETINSKAIA</p>
+                        </div>
+                    </div>
+                    <div className={styles.description}>
+                        <div><p>/ INDEPENDENT ARBITRATOR WITH A PARTICULAR FOCUS ON COMMERCIAL, CONSTRUCTION AND CORPORATE DISPUTES /</p></div>
+                    </div>
                 </div>
             </div>
         </section>
